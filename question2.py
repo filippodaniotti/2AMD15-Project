f55f3d4a0f243010695ea764bee192f44f534f93
Klára Tauchmanová <klara.tauchmanova@gmail.com>
2023-03-06 11:15:46 +0100

    Fix duplicates and optimized the solution.
    Co-authored-by: Filippo Daniotti <filippodaniotti@users.noreply.github.com>

--- question2.py
+++ question2.py
@@ -18,11 +18,18 @@ def question2(df: DataFrame):
 
     t_values = [20.0, 50.0, 310.0, 360.0, 410.0]
     results = [query(variance_df, t) for t in t_values]
+    variance_df.unpersist()
 
     for t, res in zip(t_values, [len(row) for row in results]):
         print(f"τ={t}: {res}")
     print(f"seconds to calculate: {time.perf_counter() - start:0.2f}")
-    print(f"ids for τ-value 20: {results[0]}")
+    # with open("triplets.txt", "w") as f:
+    #     for t, r in zip(t_values, results):
+    #         f.write(f"ids for τ-value {t}\n")
+    #         for l in r:
+    #             f.write(f"\t{l}\n")
+        # for t, r in zip(t_values, results):
+        #     f.write(f"ids for τ-value {t}: {r}")
 
     if is_evaluation_enabled():
         plot(list(map(str, t_values)), [len(row) for row in results])
@@ -40,42 +47,13 @@ def calc_variances(df: DataFrame) -> DataFrame:
 
     df_with_arr = df.withColumn('ARR', F.array(
         df.columns[1:])).select('_1', 'ARR')
-    # return df_with_arr \
-    #     .crossJoin(df_with_arr.selectExpr('_1 as _2', 'ARR as ARR2'))\
-    #     .filter('_1 != _2')\
-    #     .withColumn('ARR_AGG_1', agg_udf(F.array('ARR', 'ARR2')))\
-    #     .select('_1', '_2', 'ARR_AGG_1')\
-    #     .crossJoin(df_with_arr.selectExpr('_1 as _3', 'ARR as ARR3'))\
-    #     .filter('_1 != _3')\
-    #     .withColumn('full_id', F.sort_array(F.array('_1', '_2', '_3')))\
-    #     .drop_duplicates(['full_id'])\
-    #     .withColumn('AGG', agg_udf(F.array('ARR_AGG_1', 'ARR3')))\
-    #     .withColumn(
-    #         'var',
-    #         var_udf(F.col('AGG'))
-    #     ).select('full_id', 'var')\
-    #     .persist(StorageLevel.MEMORY_ONLY)
-    # return df_with_arr \
-    #     .crossJoin(df_with_arr.toDF('_2', 'ARR2'))\
-    #     .filter('_1 != _2')\
-    #     .withColumn('ARR_AGG_1', agg_udf(F.array('ARR', 'ARR2')))\
-    #     .select('_1', '_2', 'ARR_AGG_1')\
-    #     .crossJoin(df_with_arr.toDF('_3', 'ARR3'))\
-    #     .filter('_1 != _3')\
-    #     .withColumn('full_id', F.sort_array(F.array('_1', '_2', '_3')))\
-    #     .drop_duplicates(['full_id'])\
-    #     .withColumn('AGG', agg_udf(F.array('ARR_AGG_1', 'ARR3')))\
-    #     .withColumn(
-    #         'var',
-    #         var_udf(F.col('AGG'))
-    #     ).select('full_id', 'var')\
-    #     .persist(StorageLevel.MEMORY_ONLY)
+
     return df_with_arr \
-        .crossJoin(df_with_arr.toDF('_2', 'ARR2'))\
+        .crossJoin(df_with_arr.selectExpr('_1 as _2', 'ARR as ARR2'))\
         .filter('_1 < _2')\
         .withColumn('ARR_AGG_1', agg_udf(F.array('ARR', 'ARR2')))\
         .select('_1', '_2', 'ARR_AGG_1')\
-        .crossJoin(df_with_arr.toDF('_3', 'ARR3'))\
+        .crossJoin(df_with_arr.selectExpr('_1 as _3', 'ARR as ARR3'))\
         .filter('_2 < _3')\
         .withColumn('full_id', F.array('_1', '_2', '_3'))\
         .withColumn('AGG', agg_udf(F.array('ARR_AGG_1', 'ARR3')))\
