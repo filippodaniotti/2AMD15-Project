35bd57999fed88c086db844231efaabacf06b16a
Calli <me@calli.dev>
2023-03-24 03:24:56 +0100

    use better partitions in q2

--- src/2amd15/question2.py
+++ src/2amd15/question2.py
@@ -11,27 +11,26 @@ import pyspark.sql.functions as F
 
 import configuration
 from evaluation import plot
+# 16-32-64
+# 64-128-256
+# 64-64-64
 
 
 #_BEGIN_CODE
 def question2(df: DataFrame):
     print('>> executing question 2')
+    print('>> partitions: 16-32-64')
     start = time.perf_counter()
-    
+
     t_values = [20.0, 50.0, 310.0, 360.0, 410.0]
     variance_df = calc_variances(df)
-    variance410 = variance_df \
-        .filter(f'var <= {t_values[-1]}') \
-        .persist(StorageLevel.MEMORY_ONLY)
-    t410 = variance410.count()
-    results = [query(variance410, t) for t in t_values[:-1]]
-    variance410.unpersist()
+    results = [query(variance_df, t) for t in t_values]
+    variance_df.unpersist()
 
     for t, res in zip(t_values, results):
         print(f">> τ={t}: {len(res)}")
         if t in [20.0, 50.0]:
             print(f">> triples: {', '.join(res)}")
-    print(f">> τ={t_values[-1]}: {t410}") 
     print(f">> seconds to calculate: {time.perf_counter() - start:0.2f}")
 
     if configuration.ENABLE_EVALUATION:
@@ -40,14 +39,14 @@ def question2(df: DataFrame):
 
 def calc_variances(df: DataFrame) -> DataFrame:
     df_with_arr = df \
-        .withColumn('ARR', F.array(df.columns[1:])).select('_1', 'ARR') 
+        .withColumn('ARR', F.array(df.columns[1:])).select('_1', 'ARR')
 
     bc = df_with_arr.rdd.context.broadcast({
         key: np.array(value, dtype=np.int16) for (key, value)
         in df_with_arr.rdd.collectAsMap().items()
     })
 
-    df_no_arr = df.select('_1')
+    df_no_arr = df.select('_1').repartition(16)
 
     var_udf = F.udf(
         lambda row: np.var(bc.value[row[0]] + bc.value[row[1]] + bc.value[row[2]]).item(), 
@@ -56,15 +55,19 @@ def calc_variances(df: DataFrame) -> DataFrame:
 
     return df_no_arr \
         .crossJoin(df_no_arr.selectExpr('_1 as _2')) \
-        .repartition(25)\
+        .coalesce(32)\
         .filter('_1 < _2')\
         .crossJoin(df_no_arr.selectExpr('_1 as _3'))\
         .filter('_2 < _3')\
+        .coalesce(64)\
         .withColumn('full_id', F.array('_1', '_2', '_3'))\
         .withColumn(
             'var',
             var_udf(F.col('full_id'))) \
-        .select('full_id', 'var')
+        .select('full_id', 'var')\
+        .filter('var <= 410')\
+        .persist(StorageLevel.MEMORY_ONLY)
+
 
 def query(df: DataFrame, t: float) -> List[str]:
     return [
@@ -72,3 +75,4 @@ def query(df: DataFrame, t: float) -> List[str]:
         .select('full_id')
         .collect()
     ]
+
